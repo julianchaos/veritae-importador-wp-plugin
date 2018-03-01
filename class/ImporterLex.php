25cbbfeaff46f4f1c98e4ee940ab716bd1549cec
Julian Andrade <julian@ndrade.com.br>
2018-03-01 18:56:14 -0300

    Definição da query para seleção das postagens lex em suas 4 categorias de área de conhecimento (trabalho, previdência social, saude e segurança no trabalho e outros). Definição da construção do objeto de post para importação dos lex.

--- class/ImporterLex.php
+++ class/ImporterLex.php
@@ -1,13 +1,6 @@
 <?php
 
 class ImporterLex extends Importer {
-	private $_areaconhecimento = array(
-		'previdencia' => array(),
-		'sst' => array(),
-		'trabalho' => array(),
-		'outros' => array(),
-	);
-
 	function __construct() {
 		parent::__construct();
 
@@ -28,42 +21,105 @@ class ImporterLex extends Importer {
 	}
 	
 	private function loadAreaConhecimento() {
-		$this->loadAreaConhecimentoPrevidencia();
-		$this->loadAreaConhecimentoSST();
-		$this->loadAreaConhecimentoTrabalho()
+		$previdencia = $this->loadAreaConhecimentoPrevidencia();
+		$this->importLexPost($previdencia, 'Previdência Social');
+		
+		$sst = $this->loadAreaConhecimentoSST();
+		$this->importLexPost($sst, 'Segurança e Saúde no Trabalho');	
+	
+		$trabalho = $this->loadAreaConhecimentoTrabalho();
+		$this->importLexPost($trabalho, 'Trabalho');	
+		
+		$outros = $this->loadAreaConhecimentoOutros();
+		$this->importLexPost($outros, 'Outros');	
 	}
 	private function loadAreaConhecimentoPrevidencia() {
-		$query = "SELECT l.id 
-			FROM lex l JOIN previdencia p ON p.anexo_lex LIKE CONCAT('%', l.anexo) 
+		$query = "SELECT CONCAT(l.dia, '/', l.mes, '/', l.ano) as data, p.titulo, 
+				l.descricao, lt.tipo, p.anexo_lex as arquivo
+			FROM lex l 
+				JOIN previdencia p ON p.anexo_lex LIKE CONCAT('%', l.anexo) 
+				LEFT JOIN lex_tipo lt ON l.tipo_lex = lt.id
 			WHERE p.anexo_lex != '' AND l.anexo != ''";
-		$this->buildAreaConhecimentoList($query, 'previdencia');
+		
+		return $this->buildAreaConhecimentoList($query);
 	}
 	private function loadAreaConhecimentoSST(){
-		$query = "SELECT l.id 
-			FROM lex l JOIN sst s ON s.anexo_lex LIKE CONCAT('%', l.anexo) 
-			WHERE s.anexo_lex != '' AND l.anexo != ''";
-		$this->buildAreaConhecimentoList($query, 'sst');
+		$query = "SELECT CONCAT(l.dia, '/', l.mes, '/', l.ano) as data, p.titulo, 
+				l.descricao, lt.tipo, p.anexo_lex as arquivo
+			FROM lex l 
+				JOIN sst p ON p.anexo_lex LIKE CONCAT('%', l.anexo)
+				LEFT JOIN lex_tipo lt ON l.tipo_lex = lt.id
+			WHERE p.anexo_lex != '' AND l.anexo != ''";
+
+		return $this->buildAreaConhecimentoList($query);
 	}
 	private function loadAreaConhecimentoTrabalho(){
-		$query = "SELECT l.id 
-			FROM lex l JOIN trabalho t ON t.anexo_lex LIKE CONCAT('%', l.anexo) 
-			WHERE t.anexo_lex != '' AND l.anexo != ''";
-		$this->buildAreaConhecimentoList($query, 'trabalho');
+		$query = "SELECT CONCAT(l.dia, '/', l.mes, '/', l.ano) as data, p.titulo, 
+				l.descricao, lt.tipo, p.anexo_lex as arquivo 
+			FROM lex l 
+				JOIN trabalho p ON p.anexo_lex LIKE CONCAT('%', l.anexo)
+				LEFT JOIN lex_tipo lt ON l.tipo_lex = lt.id
+			WHERE p.anexo_lex != '' AND l.anexo != ''";
+		return $this->buildAreaConhecimentoList($query);
 	}
 	private function loadAreaConhecimentoOutros(){
-		$query = "SELECT l.id "
-				. "FROM lex l JOIN outros o ON o.anexo_lex LIKE CONCAT('%', l.anexo) "
-				. "WHERE o.anexo_lex != '' AND l.anexo != ''";
-		$this->buildAreaConhecimentoList($query, 'outros');
+		$query = "SELECT CONCAT(l.dia, '/', l.mes, '/', l.ano) as data, p.titulo, "
+					. "l.descricao, lt.tipo, p.anexo_lex as arquivo "
+				. "FROM lex l "
+					. "JOIN outros p ON p.anexo_lex LIKE CONCAT('%', l.anexo) "
+					. "LEFT JOIN lex_tipo lt ON l.tipo_lex = lt.id "
+				. "WHERE p.anexo_lex != '' AND l.anexo != ''";
+		return $this->buildAreaConhecimentoList($query);
 	}
-	private function buildAreaConhecimentoList($query, $area) {
+	private function buildAreaConhecimentoList($query) {
 		$result = mysqli_query($this->_conn, $query);
 		$lista = array();
 		while($row = mysqli_fetch_assoc($result)) {
-			$lista[] = $row['id'];
+			$lista[] = $row;
 		}
-
-		$this->_areaconhecimento[$area] = $lista;
+		
+		return $lista;
+	}
+	
+	private function importLexPost($lista, $area_conhecimento) {
+		echo "<p>";
+		echo "$area_conhecimento: " . count($lista);
+		echo "</p>";
+		return;
+		
+		$lex_term_id = $this->getTerm('name', 'Lex', 'tipo_postagem');
+		$area_conhecimento_term_id = $this->getTerm('name', $area_conhecimento, 'area_conhecimento');
+		
+		foreach($lista as $item) {
+			$post = array(
+				'post_date' => $this->BRDateToSystem($item['data']),
+				'post_title' => utf8_encode($item['titulo']),
+				'meta_input' => array(
+					'tipo_postagem' => $lex_term_id, //taxonomy
+					'area_conhecimento' => array($area_conhecimento_term_id), //taxonomy
+					'titulo_alternativo' => utf8_encode($item['descricao']),
+					'tipo_ato' => array($this->getTerm('name', $item['tipo'], 'tipo_ato')), //taxonomy
+//					// As informações do ato não são definidas nas postagens antigas da Veritae. Esses valores ficaram em branco para essas postagens.
+//					'numero_ato' => null, 'informacoes_ato' => null, 'ementa' => null,
+					'tipo_arquivo' => 'remoto',
+					'arquivo' => null, //Deve inserir o arquivo
+					'arquivo_url' => "http://www.veritae.com.br/" . $this->clearArquivoURL($item['arquivo']), //Deve inserir o arquivo
+					// as informações de fonte e autor do artigo não existem no Lex
+//					'fonte' => null, 'data_fonte' => null, 'autor_artigo' => null
+				),
+				'tax_input' => array(
+					'tipo_postagem' => 'Lex',
+					'area_conhecimento' => array($area_conhecimento),
+					'tipo_ato' => array($item['tipo']),
+				)
+			);
+			
+			$this->insertPost($post);
+		}
+	}
+	private function clearArquivoURL($arquivo) {
+		$output = str_replace("../", '', $anexo);
+		return $output;
 	}
 }
 
